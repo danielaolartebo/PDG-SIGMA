54fb5f544ba3eac8a033c594162e5737e01cd08e
Sebas-gifPaz777 <pazsebas797@gmail.com>
2025-04-01 23:15:46 -0500

    Revert "Fusionando cambios de draft en Front"
    This reverts commit 654462c00ae9eb2da3d6abe29f1fca8d9892269d.

--- src/TableContent.js
+++ src/TableContent.js
@@ -1,25 +1,44 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import React from 'react';
-import './TableContent.css'
+import Popup from './PopUp';
+import { useMemo } from 'react';
+import PopupCheck from './PopUpCheck';
+import Alert from './Alert';
+import { MyContext } from './MyContext';
+import { useContext } from 'react';
 
 function TableContent() {
     const [column, setColumn] = useState([]);
     const [records, setRecords] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const recordsPerPage = 8;
+    const [isOpen, setOpen] = useState(false)
+    const [isOpenCheck, setOpenCheck] = useState(false)
+    const [message, setMessage] = useState("")
+    const [idMonitoring, setIdMonitoring] = useState("")
+    const [state, setState] = useState("")
+    const [alertVisible, setAlertVisible] = useState(false);
+    const recordsPerPage = 6;
+    const { selectedValue, setSelectedValue, selectedCondition, setSelectedCondition, selectedRequest, setSelectedRequest } = useContext(MyContext)
 
     const columnNames = {
-        id: "Identificador",
-        period: "Periodo",
-        name: "Curso",
-        startPostulation: "Inicio de Convocatoria",
-        endPostulation: "Fin de Convocatoria",
+        id: "ID - CRN",
+        school: "Facultad",
+        program: "Programa",
+        course: "Curso",
+        start: "Inicio de Convocatoria",
+        finish: "Fin de Convocatoria",
+        averageGrade: "Promedio General",
+        courseGrade: "Promedio de la Materia",
+        semester: "Semestre",
         requirement: "Requisitos"
     };
 
     useEffect(() => {
-        fetch('http://localhost:3000/Data.json')
+        if(localStorage.getItem('role') === 'student'){
+            setAlertVisible(!alertVisible)
+        }
+        fetch('http://localhost:5433/monitoring/getA')
             .then(res => {
                 if (!res.ok) {
                     throw new Error(`HTTP error! Status: ${res.status}`);
@@ -27,15 +46,168 @@ function TableContent() {
                 return res.json();
             })
             .then(data => {
-                if (data.monitoria && data.monitoria.length > 0) {
-                    setColumn(Object.keys(data.monitoria[0]));
-                    setRecords(data.monitoria); 
+                if (data && data.length > 0) {
+                    setColumn(Object.keys(data[0]));
+                    setRecords(data); 
                 } else {
                     console.error("Data format is incorrect or 'monitoria' is empty.");
                 }
             })
             .catch(error => console.error('Error fetching data:', error));
     }, []);
+
+    useEffect(() => {
+        let prog ={
+
+        }
+        if ((!selectedValue && selectedCondition.trim() === "") || (selectedValue.trim() === "" && selectedCondition.trim() === "")) {
+            console.log("selectedValue está vacío, no se realizará ninguna solicitud.");
+            return; // Evita ejecutar el efecto si selectedValue es inválido
+        }
+        else if(!selectedValue  || selectedValue.trim() === ""){
+            prog={
+                programName:" ",
+                courseName:selectedCondition
+            }
+
+        }else{
+            prog={
+                programName:selectedValue,
+                courseName:selectedCondition
+            }
+        }
+        
+        if(selectedRequest === 'faculty' || selectedRequest === ''){
+            
+            fetch('http://localhost:5433/monitoring/findByFaculty',{
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(prog),
+            })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                if (data && data.length > 0) {
+                    setColumn(Object.keys(data[0]));
+                    setRecords(data); 
+                } else {
+                    console.error("Data format is incorrect or 'monitoria' is empty.");
+                }
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        }
+        else if(selectedRequest === 'program'){
+            fetch('http://localhost:5433/monitoring/findByProgram',{
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(prog),
+            })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                if (data && data.length > 0) {
+                    setColumn(Object.keys(data[0]));
+                    setRecords(data); 
+                } else {
+                    console.error("Data format is incorrect or 'monitoria' is empty.");
+                }
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        }else{
+            fetch('http://localhost:5433/monitoring/findByCourse',{
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(prog),
+            })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                if (data && data.length > 0) {
+                    setColumn(Object.keys(data[0]));
+                    setRecords(data); 
+                } else {
+                    console.error("Data format is incorrect or 'monitoria' is empty.");
+                }
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        }
+        
+    }, [selectedValue, selectedCondition]);
+
+    const handlePopUpCheck = (id,status) =>{
+        if(localStorage.getItem('role') !=='student'){
+            setMessage('Tiene que iniciar sesión como estudiante')
+            setOpen(!isOpen)
+        }else{
+            setIdMonitoring(id)
+            setState(status)
+            setOpenCheck(!isOpenCheck)
+        }
+        
+    }
+
+    const handleApplyClick = async () => {
+        setOpenCheck(!isOpenCheck);
+        console.log(localStorage.getItem('userId'))
+        const data = {
+            monitoringId:idMonitoring,
+            userId:localStorage.getItem('userId')
+        }
+        try{
+            if(state === "Activo"){
+                const response = await fetch('http://localhost:5433/monitor/create', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                  });
+                  const mess = await response.text()
+                if(response.ok){
+                    setMessage(mess)
+                    setOpen(!isOpen)
+                }
+                else{
+                    setMessage(mess)
+                    setOpen(!isOpen)
+                }  
+            }
+            else if(state === "Vencido"){
+                setMessage('El tiempo ha expirado')
+                setOpen(!isOpen)
+            }
+            else{
+                setMessage('La fecha está inactiva')
+                setOpen(!isOpen)
+            }
+            setOpen(!isOpen)
+        }
+        catch(error){
+            console.error('Error in fetching', error)
+        }
+    };
+
+    const closeAlert = () => {
+        setAlertVisible(!alertVisible);
+    };
 
     const indexOfLastRecord = currentPage * recordsPerPage;
     const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
@@ -57,18 +229,57 @@ function TableContent() {
 
     const checkStatus = (startPostulation, endPostulation) => {
         const currentDate = new Date();
-        const startDate = new Date(startPostulation.split('/').reverse().join('-'));
-        const endDate = new Date(endPostulation.split('/').reverse().join('-'));
+        const [year, month, day] = startPostulation?.split('T')[0].split("-").map(Number);
+        const [year2, month2, day2] = endPostulation?.split('T')[0].split("-").map(Number);
+        const startDate = new Date(year, month - 1, day);
+        const endDate = new Date(year2, month2 - 1, day2);
     
         if (currentDate >= startDate && currentDate <= endDate) {
             return { className: "status-active", text: "Activo" };
-        } else {
+        } else if(currentDate > startDate && currentDate> endDate){
             return { className: "status-inactive", text: "Vencido" };
+        }else{
+            return { className: "status-inactive", text: "Inactivo" };
         }
     };
 
+    const processedRecords = useMemo(() => {
+        return currentRecords.map(record => {
+            const startDateR = record.start.split('T')[0];
+            const endDateR = record.finish.split('T')[0];
+            return {
+                ...record,
+                startFormatted: startDateR,
+                endFormatted: endDateR,
+            };
+        });
+    }, [currentRecords]);
+
+    const handleClose = () =>{
+        setOpen(!isOpen);
+    }
+
+    const handleCloseCheck = () =>{
+        setOpenCheck(!isOpenCheck);
+    }
+
     return (
         <div className="main-container">
+             <Popup
+                show={isOpen}
+                onClose={() => handleClose()}
+            >
+                {message}
+            </Popup>
+            <PopupCheck
+                show={isOpenCheck}
+                onClose={() => handleCloseCheck()}
+                onApply={() => handleApplyClick()}
+            />
+             <Alert
+                show={alertVisible}
+                onClose={closeAlert}
+             />
             <div className='table-main-container'>
                 <table className="table" id="table">
                     <thead>
@@ -83,32 +294,27 @@ function TableContent() {
                         </tr>
                     </thead>
                     <tbody>
-                        {currentRecords.map((record, i) => {
-                            const status = checkStatus(record.startPostulation, record.endPostulation);
+                        {processedRecords.map((record, i) => {
+                            const status = checkStatus(record.start, record.finish);
                             return (
                                 <tr key={i}>
                                     <td className="table-data">{record.id}</td>
-                                    <td className="table-data">{record.period}</td>
-                                    <td className="table-data">{record.name}</td>
-                                    <td className="table-data">{record.startPostulation}</td>
-                                    <td className="table-data">{record.endPostulation}</td>
-                                    <td className="table-data">
-                                        <div className="requirement-container">
-                                            <button className="view-button">Ver</button>
-                                            <span className="requirement-text">
-                                                {record.requirement.split('\n').map((line, index) => (
-                                                    <span key={index}>{line}<br /></span>
-                                                ))}
-                                            </span>
-                                        </div>
-                                    </td>
+                                    <td className="table-data">{record.school.name}</td>
+                                    <td className="table-data">{record.program.name}</td>
+                                    <td className="table-data">{record.course.name}</td>
+                                    <td className="table-data">{record.startFormatted}</td>
+                                    <td className="table-data">{record.endFormatted}</td>
+                                    <td className="table-data">{record.averageGrade}</td>
+                                    <td className="table-data">{record.courseGrade}</td>
+                                    <td className="table-data">{record.semester}</td>
+                        
                                     <td className="table-data">
                                         <div className={status.className}>
                                             {status.text}
                                         </div>
                                     </td>
                                     <td className="table-data">
-                                        <div className="apply-button">Aplicar</div>
+                                        <div className="apply-button" onClick={() => handlePopUpCheck(record.id, status.text)}>Aplicar</div>
                                     </td>
                                 </tr>
                             );
--- src/VerticalNavbar.js
+++ src/VerticalNavbar.js
@@ -1,47 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../src/img/logo2.png";
-import './VerticalNavbar.css';
 
 function VerticalNavbar() {
 
   const handleClose= () =>{
-    localStorage.setItem('role','');
-    localStorage.setItem('userId','');
-  };
-
+    localStorage.setItem('role','')
+    localStorage.setItem('userId','')
+  }
   return (
     <div className="vertical-navbar">
-      {/* Logo starts */}
-      <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
-      {/* Logo ends */}
-
       <div className="menu-items">
-        <NavLink to="/Task" className={({ isActive }) => isActive ? "active" : ""}>Actividades</NavLink>
-        <NavLink to="/Profile" className={({ isActive }) => isActive ? "active" : ""}>Mi perfil</NavLink>
-        <NavLink to="/CreateMonitoria" className={({ isActive }) => isActive ? "active" : ""}>Crear monitoria</NavLink>
-        <NavLink to="/ApplyMonitor" className={({ isActive }) => isActive ? "active" : ""}>Postulaciones</NavLink>
-        <NavLink to="/Applicants" className={({ isActive }) => isActive ? "active" : ""}>Mis postulantes</NavLink>
-        <NavLink to="/Reports" className={({ isActive }) => isActive ? "active" : ""}>Reportes</NavLink>
-
-        <div className="logout-container">
-          <NavLink 
-            to="/" 
-            className={({ isActive }) => isActive ? "logout-button active" : "logout-button"} 
-            onClick={handleClose}
-          >
-            Cerrar sesión
-          </NavLink>
-        </div>
+        <NavLink to="/Login" className="logout-button" activeClassName="active">
+          Cerrar sesión
+        </NavLink>
+        <NavLink to="/Task" activeClassName="active">Actividades</NavLink>
+        <NavLink to="/Profile" activeClassName="active">Mi perfil</NavLink>
+        <NavLink to="/CreateMonitoria" activeClassName="active">Crear monitoria</NavLink>
+        <NavLink to="/" activeClassName="active">Postulaciones</NavLink>
+        <NavLink to="/Applicants" activeClassName="active">Mis postulantes</NavLink>
+        <NavLink to="/Reports" activeClassName="active">Reportes</NavLink>
       </div>
     </div>
   );
 }
 
 export default VerticalNavbar;
-
 
 
 
