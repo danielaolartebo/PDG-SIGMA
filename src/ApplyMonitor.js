0bb67fc6438ed96c46a0461827985a77c0224f46
Sebas-gifPaz777 <pazsebas797@gmail.com>
2025-04-01 23:18:20 -0500

    Reapply "Fusionando cambios de draft en Front"
    This reverts commit 54fb5f544ba3eac8a033c594162e5737e01cd08e.

---
+++ src/ApplyMonitor.js
@@ -0,0 +1,31 @@
+import './ApplyMonitor.css';
+import React from 'react';
+import VerticalNavbar from './VerticalNavbar';
+import TableContent from './TableContent';
+import Dropdown from './Filters';
+
+function ApplyMonitor() {
+    console.log("✅ ApplyMonitor se está renderizando");
+
+    return (
+
+            <div className="apply-monitor-container">
+                <VerticalNavbar />
+
+                <div className="title-container-apply-monitor">
+                    <div className="title-apply-monitor">Postulación a Monitor</div>
+                </div>
+
+                <div className="apply-monitor-cont">
+                        <Dropdown />
+                     <TableContent />
+                </div>
+            </div>
+
+    );
+}
+
+export default ApplyMonitor;
+
+
+  
