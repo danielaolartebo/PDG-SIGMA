8fc63b2b79f1c78e85bf59fcec2bf8dea906bfd7
Daniela Olarte <danielaolarte123@gmail.com>
2025-05-12 18:49:35 -0500

    piechart reload

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+if (typeof window.isNumber === 'undefined') {
+  window.isNumber = function (value) {
+    return typeof value === 'number' && !isNaN(value);
+  };
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
