e006b6ee0d364fe144f2d1950ee7008cfe49438b
Daniela Olarte <danielaolarte123@gmail.com>
2025-05-12 18:54:30 -0500

    recharts reload

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-if (typeof window.isNumber === 'undefined') {
-  window.isNumber = function (value) {
-    return typeof value === 'number' && !isNaN(value);
-  };
-}
+globalThis.isNumber = globalThis.isNumber || function (value) {
+  return typeof value === 'number' && !isNaN(value);
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
