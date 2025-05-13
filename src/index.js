f1e4caa678115d4b9e29a6e7633b8691969903fc
Daniela Olarte <danielaolarte123@gmail.com>
2025-05-13 18:26:58 -0500

    new file added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
+import './globalFix';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-globalThis.isNumber = globalThis.isNumber || function (value) {
-  return typeof value === 'number' && !isNaN(value);
-};
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <App />
