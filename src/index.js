c89c1612e37fb24ea02dde4072c1cb6896ea2d1b
Daniela Olarte <danielaolarte123@gmail.com>
2025-05-13 18:15:19 -0500

    isNumber

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+globalThis.isNumber = globalThis.isNumber || function (value) {
+  return typeof value === 'number' && !isNaN(value);
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
